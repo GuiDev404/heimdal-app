10c0ef7834619042b4a32cb402eaedf2f4e9edba
Guido.R <guidorivoira@hotmail.com>
2024-04-15 22:45:35 -0300

    feat: actualiza tipo Position en hook useGeolocation

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -1,16 +1,17 @@
 import { useEffect, useState } from 'react'
-// import useSWR from 'swr'
-// import { getGeolocationAproximate } from '../services'
 import { Coord } from '../types'
 
-export interface GeoApproximate {
-  status: string;
-  country: string;
-  countryCode: string;
-  regionName: string;
-  city: string;
-  lat: number;
-  lon: number;
+interface Position {
+  coords: {
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+    altitude: number | null;
+    altitudeAccuracy: number | null;
+    heading: number | null;
+    speed: number | null;
+  };
+  timestamp: number;
 }
 
 interface useGeolocationResult {
@@ -27,38 +28,6 @@ const DEFAULT_COORDS: Coord = {
 const useGeolocation = (): useGeolocationResult => {
   const [coords, setCoords] = useState<Coord | null>(DEFAULT_COORDS)
 
-  // const isEnabled = !actualLocation ? 'geolocation' : null
-  // const { data, isLoading } = useSWR<GeoApproximate>(isEnabled, getGeolocationAproximate, {
-  //   // revalidateOnMount: false,
-  //   revalidateOnFocus: false
-  // })
-
-  // useEffect(() => {
-  //   if (!isLoading && data) {
-  //     setCoords({ lat: data.lat, lon: data.lon })
-  //   }
-  // }, [isLoading, data])
-
-  // useEffect(() => {
-  //   const options = {
-  //     enableHighAccuracy: true, // el navegador intentará obtener la ubicación con la mayor precisión posible
-  //     // timeout: 5000,
-  //     maximumAge: 0 // el navegador no usará ubicaciones antiguas en cache y intentará obtener una nueva ubicación
-  //   }
-
-  //   navigator.geolocation.getCurrentPosition(
-  //     (pos) => {
-  //       const crd = pos.coords
-
-  //       setCoords({ lat: crd.latitude, lon: crd.longitude })
-  //     },
-  //     () => {
-  //       setCoords(DEFAULT_COORDS)
-  //     },
-  //     options
-  //   )
-  // }, [])
-
   useEffect(() => {
     const options = {
       enableHighAccuracy: true, // el navegador intentará obtener la ubicación con la mayor precisión posible
@@ -66,7 +35,7 @@ const useGeolocation = (): useGeolocationResult => {
       maximumAge: 0 // el navegador no usará ubicaciones antiguas en cache y intentará obtener una nueva ubicación
     }
 
-    const successCb = (pos) => {
+    const successCb = (pos: Position) => {
       const crd = pos.coords
       setCoords({ lat: crd.latitude, lon: crd.longitude })
     }
